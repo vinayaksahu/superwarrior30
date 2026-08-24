66aaa8cb067e401147acb6d3600328bc83517e7a
Vinayak Sahu <vinayaksahu3@gmail.com>
2026-08-25 02:55:58 +0530

    fix: add datasource url to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,4 +6,7 @@ import "dotenv/config";
 
 export default defineConfig({
   schema: path.join(__dirname, "prisma", "schema.prisma"),
+  datasource: {
+    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
+  },
 });
