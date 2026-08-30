37cb4422919df3de301d3cc8b38e5f38eb7ea139
Vinayak Sahu <vinayaksahu3@gmail.com>
2026-08-30 22:19:24 +0530

    fix: update next.config.ts CSP and add force-dynamic to cashbacks redirect

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const cspHeader = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.razorpay.com https://iframe.mediadelivery.net https://cdnjs.cloudflare.com;
   worker-src 'self' blob: https://cdnjs.cloudflare.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' blob: data: https://*.b-cdn.net https://*.bunnycdn.com https://*.r2.cloudflarestorage.com;
+  img-src 'self' blob: data: https://*.b-cdn.net https://*.bunnycdn.com https://*.r2.cloudflarestorage.com https://api.qrserver.com;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' blob: data: https://video.bunnycdn.com https://*.b-cdn.net https://*.bunnycdn.com https://*.mediadelivery.net https://api.razorpay.com https://checkout.razorpay.com https://*.upstash.io https://cdnjs.cloudflare.com;
   media-src 'self' blob: data: https://*.b-cdn.net https://*.bunnycdn.com https://*.mediadelivery.net;
